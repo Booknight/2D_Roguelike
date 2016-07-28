ee94908719bf3c371248513faf1267b7d1d07c63
SwordsManNick <nicholasbyland@yahoo.com>
2016-07-28 16:33:39 -0500

    2D_Roguelike

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
+	public float turnDelay = .1f;
 	public static GameManager instance = null;
 	public BoardManager boardScript;
+	public int playerFoodPoints = 100;
+	[HideInInspector] public bool playersTurn = true;
+
 
 	private int level = 3;
+	private List <Enemy> enemies;
+	private bool enemiesMoving;
+
 
 	// Use this for initialization
 	void Awake ()
@@ -17,13 +25,20 @@ public class GameManager : MonoBehaviour {
 			Destroy (gameObject);
 
 		DontDestroyOnLoad (gameObject);
+		enemies = new List<Enemy> ();
 		boardScript = GetComponent<BoardManager> ();
 		InitGame ();
 	}
 
 	void InitGame()
 	{
+		enemies.Clear ();
 		boardScript.SetupScene (level);
+	}
+
+	public void GameOver()
+	{
+		enabled = false;
 	}
 
 	// Update is called once per frame
